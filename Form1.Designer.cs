3abc2b54f5ca8fa64dba855e6c81e344cb47f7d5
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2023-10-19 07:59:08 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace WindowDvd
+{
+    partial class DvdLogo
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PosLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PosLabel
+            // 
+            this.PosLabel.AutoSize = true;
+            this.PosLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PosLabel.Location = new System.Drawing.Point(12, 9);
+            this.PosLabel.Name = "PosLabel";
+            this.PosLabel.Size = new System.Drawing.Size(42, 17);
+            this.PosLabel.TabIndex = 1;
+            this.PosLabel.Text = "(0, 0)";
+            this.PosLabel.UseWaitCursor = true;
+            // 
+            // DvdLogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::WindowDvd.Properties.Resources.dvdlogo;
+            this.ClientSize = new System.Drawing.Size(184, 111);
+            this.Controls.Add(this.PosLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DvdLogo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.DvdLogo_Load);
+            this.Click += new System.EventHandler(this.DvdLogo_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label PosLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowDvd
+{
+    public partial class DvdLogo : Form
+    {
+        bool RunBool = false;
+
+        public DvdLogo()
+        {
+            InitializeComponent();
+        }
+
+        public void ChangePosThread()
+        {
+            int ScreenW = Screen.PrimaryScreen.Bounds.Width - Width;
+            int ScreenH = Screen.PrimaryScreen.Bounds.Height - Height;
+            int speedX = 1;
+            int speedY = 1;
+            bool collision = false;
+            this.Controls.Remove(PosLabel);
+            while (true)
+            {
+                // PosLabel.Text = $"({Top}; {Left})";
+                if (RunBool)
+                {
+                    if (Left >= ScreenW || Left <= 0)
+                    {
+                        speedX = -speedX;
+                        BackColor = Color.Red;
+                        collision = true;
+                    }
+                    if (Top >= ScreenH || Top <= 0)
+                    {
+                        speedY = -speedY;
+                        if (collision) { BackColor = Color.Green; }
+                        else { BackColor = Color.Blue; }
+                    }
+                    collision = false;
+                    Left += speedX;
+                    Top += speedY;
+                }
+            }
+        }
+
+        public async void DvdLogo_Load(object sender, EventArgs e)
+        {
+            await Task.Run(() => ChangePosThread());
+        }
+
+        private void DvdLogo_Click(object sender, EventArgs e)
+        {
+            RunBool = !RunBool;
+        }
+    }
+}
